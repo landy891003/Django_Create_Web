01dfa419d6e06f60aaee2e26b46ea6fa53567259
landy891003 <landy891003@gmail.com>
2021-05-30 05:51:46 +0800

    hw8

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=sp*o8_*r48wx23he$sztq4%0g!_z@7&=(+@m)iy#_&tue7etk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'web-hw8.herokuapp.com']
 
 
 # Application definition
