56b1c9ad84213f5aa30980fb2782c7bce1612018
landy891003 <landy891003@gmail.com>
2021-05-30 06:17:00 +0800

    hw8

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -130,24 +130,21 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CJ22LOGGING = {
+LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'file':
-            {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': './debug.log',
-            },
-    },
-    'loggers':
-        {
-            'django':
-                {
-                    'handlers': ['file'],
-                    'level': 'DEBUG',
-                    'propagate': True,
-                },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './debug.log',
         },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
 }
